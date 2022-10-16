1ebec7b27badd28247b3634ec1cd5efb7058c023
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2022-10-16 20:21:13 +0200

    Update mq2.c
    Añadidos cambios para funcionar con MQTT

--- MESPF_Publisher/gpios/mq2.c
+++ MESPF_Publisher/gpios/mq2.c
@@ -10,20 +10,34 @@
 #include <stdbool.h>
 
 #include "esp_log.h"
+#include "string.h"
 
 static const char TAG[] = "mq2";
 
 bool g_mq2_initialized 	= false;
 
+static sensor_data_t sensor_data;
+
 void mq2_init(void){
+
+	int number_of_values = 1;
 
 	adc1_config_width(width);
 	adc1_config_channel_atten(channel, atten);
 
+	strcpy(sensor_data.sensorName, "MQ2");
+
+	sensor_data.valuesLen = number_of_values;
+
+	sensor_data.sensor_values = (sensor_value_t *)malloc(sizeof(sensor_value_t) * number_of_values);
+
+	strcpy(sensor_data.sensor_values[0].valueName, "Smoke/Gas");
+	sensor_data.sensor_values[0].sensor_value_type = INTEGER;
+
 	g_mq2_initialized = true;
 }
 
-int mq2_get_data(void){
+sensor_data_t mq2_get_sensor_data(void){
 
 	int read_raw = 0;
 
@@ -33,11 +47,11 @@ int mq2_get_data(void){
         	read_raw += adc1_get_raw(channel);
         }
 
-        read_raw /= N_SAMPLES;
+        sensor_data.sensor_values[0].sensor_value.ival = read_raw / N_SAMPLES;
 	}
 	else{
 		ESP_LOGE(TAG, "Error, you can't operate with the MQ2 without initializing the ADC channel");
 	}
 
-	return read_raw;
+	return sensor_data;
 }
