677b8d1d896fafd66272c075d75a57b3444887d3
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2022-10-19 20:34:34 +0200

    Add files via upload
    Driver del periférico DHT22: Testeado y funcionando correctamente con MQTT

---
+++ MESPF_Publisher/gpios/dht22.c
@@ -0,0 +1,169 @@
+/*
+ * dht22.c
+ *
+ *  Created on: 19 oct. 2022
+ *      Author: Kike
+ */
+
+#include "dht22.h"
+
+#include <stdbool.h>
+
+#include "unistd.h"
+#include "esp_log.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+static const char TAG[]  = "dht22";
+
+bool g_dht22_initialized = false;
+
+static sensor_data_t sensor_data;
+
+/**
+ * Function to check income signal level
+ * @param usTimeOut, max microseconds to be considered communication error
+ * @param state, expected income signal level
+ * @return number of microseconds passed in the expected state, -1 if timeout occurs
+ */
+static int dht22_get_signal_level(int usTimeOut, bool state){
+
+	int uSec = 0;
+	
+	while(gpio_get_level(DHT22_GPIO) == state){
+
+		if(uSec > usTimeOut)
+			return -1;
+		
+		uSec++;
+		usleep(1);
+	}
+	
+	return uSec;
+}
+
+void dht22_init(void){
+
+	int number_of_values = 2;
+
+	strcpy(sensor_data.sensorName, "DHT22");
+
+	sensor_data.valuesLen = number_of_values;
+
+	sensor_data.sensor_values = (sensor_value_t *)malloc(sizeof(sensor_value_t) * number_of_values);
+
+	strcpy(sensor_data.sensor_values[0].valueName, "Humidity");
+	sensor_data.sensor_values[0].sensor_value_type = FLOAT;
+
+	strcpy(sensor_data.sensor_values[1].valueName, "Temperature");
+	sensor_data.sensor_values[1].sensor_value_type = FLOAT;
+
+	g_dht22_initialized = true;
+}
+
+sensor_data_t dht22_get_sensor_data(void){
+
+	int uSec;
+
+	uint8_t dhtData[DHT22_MAX_DATA];
+	uint8_t byteInx = 0;
+	uint8_t bitInx = 7;
+
+	float humidity = 0.0;
+	float temperature = 0.0;
+
+	sensor_data.sensor_values[0].sensor_value.fval = 0.0;
+	sensor_data.sensor_values[1].sensor_value.fval = 0.0;
+
+	if(g_dht22_initialized){
+
+		for (int k = 0; k < DHT22_MAX_DATA; k++)
+			dhtData[k] = 0;
+
+		// Send start signal to DHT sensor
+		gpio_set_direction(DHT22_GPIO, GPIO_MODE_OUTPUT);
+
+		// Pull down for 3 ms for a smooth and nice wake up
+		gpio_set_level(DHT22_GPIO, 0);
+		usleep(3000);
+
+		// Pull up for 25 us for a gentile asking for data
+		gpio_set_level(DHT22_GPIO, 1);
+		usleep(25);
+
+		gpio_set_direction(DHT22_GPIO, GPIO_MODE_INPUT);	// change to input mode
+
+		// DHT will keep the line low for 80 us and then high for 80 us
+		uSec = dht22_get_signal_level(85, 0);
+	//	ESP_LOGI( TAG, "Response = %d", uSec );
+		if(uSec < 0){
+			return sensor_data;
+		}
+
+		uSec = dht22_get_signal_level(85, 1);
+	//	ESP_LOGI( TAG, "Response = %d", uSec );
+		if(uSec < 0){
+			return sensor_data;
+		}
+
+		// No errors, read the 40 data bits
+		for(int k = 0; k < 40; k++){
+
+			// Starts new data transmission with >50us low signal
+			uSec = dht22_get_signal_level(56, 0);
+			if(uSec < 0){
+				return sensor_data;
+			}
+
+			// Check to see if after >70us RX data is a 0 or a 1
+			uSec = dht22_get_signal_level(75, 1);
+			if(uSec < 0){
+				return sensor_data;
+			}
+
+			// Add the current read to the output data.
+			// Since all dhtData array where set to 0 at the start,
+			// only look for "1" (>28us us)
+			if (uSec > 40){
+				dhtData[byteInx] |= (1 << bitInx);
+			}
+
+			// Index to next byte
+			if (bitInx == 0){
+				bitInx = 7;
+				byteInx++;
+			}
+			else{
+				bitInx--;
+			}
+		}
+
+		// Get humidity from Data[0] and Data[1]
+		humidity = dhtData[0];
+		humidity *= 0x100;					// >> 8
+		humidity += dhtData[1];
+		humidity /= 10;						// get the decimal
+
+		// Get temperature from Data[2] and Data[3]
+		temperature = dhtData[2] & 0x7F;
+		temperature *= 0x100;				// >> 8
+		temperature += dhtData[3];
+		temperature /= 10;					// get the decimal
+
+		if(dhtData[2] & 0x80) 				// negative temperature
+			temperature *= -1;
+
+		// Verify if checksum is OK
+		// Checksum is the sum of Data 8 bits masked out 0xFF
+		if (dhtData[4] == ((dhtData[0] + dhtData[1] + dhtData[2] + dhtData[3]) & 0xFF)){
+			sensor_data.sensor_values[0].sensor_value.fval = humidity;
+			sensor_data.sensor_values[1].sensor_value.fval = temperature;
+		}
+
+	}
+	else{
+		ESP_LOGE(TAG, "Error, you can't operate with the DHT22 without initializing it");
+	}
+	
+	return sensor_data;
+}
---
+++ MESPF_Publisher/gpios/dht22.h
@@ -0,0 +1,63 @@
+/*
+ * dht22.h
+ *
+ *  Created on: 19 oct. 2022
+ *      Author: Kike
+ */
+
+/**
+ * [Note] DHT22 Documentation:
+ *
+ * DATA: Hum = 16 bits, Temp = 16 Bits, check-sum = 8 Bits
+ *
+ * Example: MCU has received 40 bits data from AM2302 as
+ * 0000 0010 1000 1100 0000 0001 0101 1111 1110 1110
+ * 16 bits RH data + 16 bits T data + check sum
+ *
+ * 1) we convert 16 bits RH data from binary system to decimal system, 0000 0010 1000 1100 → 652
+ * Binary system Decimal system: RH=652/10=65.2%RH
+ *
+ * 2) we convert 16 bits T data from binary system to decimal system, 0000 0001 0101 1111 → 351
+ * Binary system Decimal system: T=351/10=35.1°C
+ *
+ * When highest bit of temperature is 1, it means the temperature is below 0 degree Celsius.
+ * Example: 1000 0000 0110 0101, T= minus 10.1°C: 16 bits T data
+ *
+ * 3) Check Sum=0000 0010+1000 1100+0000 0001+0101 1111=1110 1110 Check-sum=the last 8 bits of Sum=11101110
+ *
+ * Signal & Timings:
+ *
+ * The interval of whole process must be beyond 2 seconds.
+ *
+ * To request data from DHT:
+ *
+ * 1) Sent low pulse for > 1~10 ms (MILI SEC)
+ * 2) Sent high pulse for > 20~40 us (Micros).
+ * 3) When DHT detects the start signal, it will pull low the bus 80us as response signal,
+ *    then the DHT pulls up 80us for preparation to send data.
+ * 4) When DHT is sending data to MCU, every bit's transmission begin with low-voltage-level that last 50us,
+ *    the following high-voltage-level signal's length decide the bit is "1" or "0".
+ *    0: 26~28 us
+ *    1: 70 us
+ */
+
+#ifndef MAIN_GPIOS_DHT22_H_
+#define MAIN_GPIOS_DHT22_H_
+
+#include "recollecter.h"
+
+#define DHT22_GPIO				25
+
+#define DHT22_MAX_DATA 			5
+
+/*
+ * Initializes DTH22 peripheral
+ */
+void dht22_init(void);
+
+/**
+ * Gets data
+ */
+sensor_data_t dht22_get_sensor_data(void);
+
+#endif /* MAIN_GPIOS_DHT22_H_ */
