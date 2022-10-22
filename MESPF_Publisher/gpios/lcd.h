7cd2825c7ab8c5c1501806895984a3a844ba5533
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2022-10-22 23:04:45 +0200

    Update lcd.h
    Ahora muestra la información relativa a los periféricos, también he cambiado la funcionalidad de autoscroll por la de poder mover el display a la izquierda o la derecha (lo vamos a utilizar mucho más).

--- MESPF_Publisher/gpios/lcd.h
+++ MESPF_Publisher/gpios/lcd.h
@@ -9,6 +9,7 @@
 #define MAIN_GPIOS_LCD_H_
 
 #include "driver/i2c.h"
+#include "recollecter.h"
 
 #define LCD_SDA_GPIO			21
 #define LCD_SCL_GPIO			22
@@ -19,6 +20,9 @@
 #define LCD_ADDRESS				0x27		// Default address
 
 #define MAX_TICKS_TO_WAIT		1000		// 1000 ms -> 1 s
+#define MAX_CHARAC_ROW			40			// Only 16 are visible at once (per row)
+
+#define LCD_SENSOR_SIZE 		5			// Should be the same number as RECOLLECTER_SIZE in "recollecter.h"
 
 /**
  * [Note] 8 bits: bit 7 => D7 / bit 6 => D6 / bit 5 => D5 / bit 4 => D4 / bit 3 => LED+ / bit 2 => en / bit 1 => R/W / bit 0 => RS
@@ -29,6 +33,7 @@
 #define LCD_RETURNHOME 			0x02		// Move cursor to first row and first column (this command don't clean the display)
 #define LCD_ENTRYMODESET 		0x04		// Change how the cursor works
 #define LCD_DISPLAYCONTROL 		0x08		// Change how the display works
+#define LCD_CURSORSHIFT 		0x10		// Force the movement of the cursor/display
 #define LCD_FUNCTIONSET 		0x20		// Change how the LCD controller works
 
 // Flags for display entry mode
@@ -44,6 +49,12 @@
 #define LCD_CURSOROFF 			0x00
 #define LCD_BLINKON 			0x01
 #define LCD_BLINKOFF 			0x00
+
+// Flags for display/cursor shift
+#define LCD_DISPLAYMOVE 		0x08
+#define LCD_CURSORMOVE 			0x00
+#define LCD_MOVERIGHT 			0x04
+#define LCD_MOVELEFT 			0x00
 
 // Flags for function set
 #define LCD_4BITMODE 			0x00
@@ -106,14 +117,14 @@ void lcd_left_to_right(void);
 void lcd_right_to_left(void);
 
 /**
- * Turn on the auto scroll
+ * Shift the display to the Left
  */
-void lcd_autoscroll_on(void);
+void lcd_shift_display_left(void);
 
 /**
- * Turn off the auto scroll
+ * Shift the display to the Right
  */
-void lcd_autoscroll_off(void);
+void lcd_shift_display_right(void);
 
 /**
  * Set the cursor in the selected row and column
@@ -128,5 +139,28 @@ void lcd_set_cursor(int row, int col);
  * @param *str, char pointer to the string message you want to show on the display
  */
 void lcd_write(char *str);
+
+/**
+ * Indicates that one sensor is added to the system
+ */
+void lcd_one_more_sensor(void);
+
+/**
+ * Indicates that one sensor is removed from the system
+ * @param sensor_id literally the ID of the sensor to be removed
+ */
+void lcd_one_less_sensor(int sensor_id);
+
+/**
+ * Updates the values of the given sensor
+ * @param sensor_id literally the ID of the sensor to be updated
+ * @param sensor_data the new values
+ */
+void lcd_update_sensor_data(int sensor_id, sensor_data_t sensor_data);
+
+/**
+ * Show on the LCD the data of the next sensor
+ */
+void lcd_show_next_sensor_data(void);
 
 #endif /* MAIN_GPIOS_LCD_H_ */
