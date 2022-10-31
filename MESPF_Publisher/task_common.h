d912ae23e55fa1e38f6989b0e7448264575beb3f
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2022-10-31 18:43:20 +0100

    Update task_common.h
    Añadido información para los tasks de los sensores.

--- MESPF_Publisher/task_common.h
+++ MESPF_Publisher/task_common.h
@@ -26,6 +26,18 @@
 
 #define MQTT_APP_DATA_SENDER_STACK_SIZE 4096
 #define MQTT_APP_DATA_SENDER_PRIORITY	5
-#define MQTT_APP_DATA_SENDER_CORE_ID	1
+#define MQTT_APP_DATA_SENDER_CORE_ID	0
+
+#define LCD_STACK_SIZE					4096
+#define LCD_PRIORITY					3
+#define LCD_CORE_ID						1
+
+#define DHT22_STACK_SIZE				4096
+#define DHT22_PRIORITY					4
+#define DHT22_CORE_ID					1
+
+#define MQ2_STACK_SIZE					4096
+#define MQ2_PRIORITY					5
+#define MQ2_CORE_ID						1
 
 #endif /* MAIN_TASK_COMMON_H_ */
