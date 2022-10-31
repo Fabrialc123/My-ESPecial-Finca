d7827063a2af7358fe134fb5afb5328c760dfa9b
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2022-10-31 19:13:55 +0100

    Update mq2.c
    Cambio para funcionar con task

--- MESPF_Publisher/gpios/mq2.c
+++ MESPF_Publisher/gpios/mq2.c
@@ -6,8 +6,10 @@
  */
 
 #include "gpios/mq2.h"
+#include "task_common.h"
 
 #include <stdbool.h>
+#include <pthread.h>
 
 #include "esp_log.h"
 #include "string.h"
@@ -16,42 +18,79 @@ static const char TAG[] = "mq2";
 
 bool g_mq2_initialized 	= false;
 
-static sensor_data_t sensor_data;
+static int read_raw;
+
+static pthread_mutex_t mutex_mq2;
+
+/**
+ * Task for MQ2
+ */
+
+static void mq2_task(void *pvParameters){
+
+	int auxRead_raw;
+
+	for(;;){
+
+		pthread_mutex_lock(&mutex_mq2);
+
+		auxRead_raw = 0;
+
+        for (int i = 0; i < N_SAMPLES; i++) {
+        	auxRead_raw += adc1_get_raw(channel);
+        }
+
+        read_raw = auxRead_raw / N_SAMPLES;
+
+        pthread_mutex_unlock(&mutex_mq2);
+
+        vTaskDelay(MQ2_TIME_TO_UPDATE_DATA);
+	}
+}
 
 void mq2_init(void){
 
-	int number_of_values = 1;
+	if(pthread_mutex_init(&mutex_mq2, NULL) != 0){
+		ESP_LOGE(TAG,"Failed to initialize the MQ2 mutex");
+	}
+	else{
 
-	adc1_config_width(width);
-	adc1_config_channel_atten(channel, atten);
+		adc1_config_width(width);
+		adc1_config_channel_atten(channel, atten);
 
-	strcpy(sensor_data.sensorName, "MQ2");
+		read_raw = 0;
 
-	sensor_data.valuesLen = number_of_values;
+		xTaskCreatePinnedToCore(&mq2_task, "mq2_task", MQ2_STACK_SIZE, NULL, MQ2_PRIORITY, NULL, MQ2_CORE_ID);
 
-	sensor_data.sensor_values = (sensor_value_t *)malloc(sizeof(sensor_value_t) * number_of_values);
-
-	strcpy(sensor_data.sensor_values[0].valueName, "S/G");
-	sensor_data.sensor_values[0].sensor_value_type = INTEGER;
-
-	g_mq2_initialized = true;
+		g_mq2_initialized = true;
+	}
 }
 
 sensor_data_t mq2_get_sensor_data(void){
 
-	int read_raw = 0;
+	sensor_data_t aux;
+	sensor_value_t *aux2;
+	int number_of_values = 1;
 
-	if(g_mq2_initialized){
-
-        for (int i = 0; i < N_SAMPLES; i++) {
-        	read_raw += adc1_get_raw(channel);
-        }
-
-        sensor_data.sensor_values[0].sensor_value.ival = read_raw / N_SAMPLES;
-	}
-	else{
-		ESP_LOGE(TAG, "Error, you can't operate with the MQ2 without initializing the ADC channel");
+	if(!g_mq2_initialized){
+		ESP_LOGE(TAG, "Error, you can't operate with the MQ2 without initializing it");
+		aux.valuesLen = 0;
+		return aux;
 	}
 
-	return sensor_data;
+	pthread_mutex_lock(&mutex_mq2);
+
+	aux2 = (sensor_value_t *)malloc(sizeof(sensor_value_t) * number_of_values);
+
+	strcpy(aux.sensorName, "MQ2");
+	aux.valuesLen = number_of_values;
+	aux.sensor_values = aux2;
+
+	strcpy(aux.sensor_values[0].valueName,"S/G");
+	aux.sensor_values[0].sensor_value_type = INTEGER;
+	aux.sensor_values[0].sensor_value.ival = read_raw;
+
+	pthread_mutex_unlock(&mutex_mq2);
+
+	return aux;
 }
