2d6d6c3858ad0c3251f6c60fdbf0c44f9a1f6343
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2022-11-04 13:01:00 +0100

    Update mq2.h
    Cambio de nombre en las variables

--- MESPF_Publisher/gpios/mq2.h
+++ MESPF_Publisher/gpios/mq2.h
@@ -13,9 +13,9 @@
 
 #define N_SAMPLES   	64          						//Used for minimizing noise
 
-static const adc1_channel_t channel = ADC1_CHANNEL_6;     	//ADC1 channel 6 -> GPIO34
-static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
-static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc1_channel_t channel_mq2 = ADC1_CHANNEL_6;     	//ADC1 channel 6 -> GPIO34
+static const adc_bits_width_t width_mq2 = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten_mq2 = ADC_ATTEN_DB_11;
 
 #define MQ2_TIME_TO_UPDATE_DATA	100 						// 1 second = 100 ticks
 
