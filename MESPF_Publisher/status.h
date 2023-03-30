745f6104c43de6cbf9b11738ab6cd376181a356a
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2023-03-30 22:01:03 +0200

    Update status.h

--- MESPF_Publisher/status.h
+++ MESPF_Publisher/status.h
@@ -25,8 +25,8 @@ int status_setDateTime(const char *date,const char *time);
 void status_ntp_get_conf(char *server, unsigned int *sync_interval, short int *status);
 void status_ntp_set_conf(const char *server, const unsigned int sync_interval);
 
-sensor_data_t status_recollecter (void);
-
-
+sensor_data_t* status_recollecter (int* number_of_sensors);
+sensor_gpios_info_t* status_gpios_recollecter (int* number_of_sensors);
+sensor_additional_parameters_info_t* status_parameters_recollecter (int* number_of_sensors);
 
 #endif /* MAIN_STATUS_H_ */
