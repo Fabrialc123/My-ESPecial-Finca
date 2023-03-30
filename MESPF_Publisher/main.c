4e48c8a63fe95d9fec4d0a9b6bf71ce6adf9106f
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2023-03-30 21:11:05 +0200

    Update main.c

--- MESPF_Publisher/main.c
+++ MESPF_Publisher/main.c
@@ -6,32 +6,35 @@
 
 
 #include <mqtt/mqtt_app.h>
-
 #include <nvs_app.h>
 #include <wifi_app.h>
 #include <http_server.h>
 #include <esp_log.h>
 #include <recollecter.h>
 #include <status.h>
-
+#include <gpios_manager.h>
+#include <sensors_manager.h>
+#include <gpios/lcd.h>
 
 static const char main_TAG[] = "MAIN";
 
 
 void app_main(void)
 {
-
 	nvs_app_init();
+
     ESP_LOGI(main_TAG, "[APP] Startup..");
     ESP_LOGI(main_TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
     ESP_LOGI(main_TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-	wifi_app_start();
-
 	recollecter_start();
-
+	gpios_manager_init();
+    sensors_manager_init();
+	
 	status_start();
+	sensors_manager_sensors_startup();
+	lcd_init();
 
+	wifi_app_start();
 	mqtt_app_start();
 }
-
