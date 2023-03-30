6afe12b969847cb8561a95f5fa2f12fc588db135
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2023-03-31 00:58:44 +0200

    Update main.c

--- MESPF_Publisher/main.c
+++ MESPF_Publisher/main.c
@@ -22,19 +22,19 @@ static const char main_TAG[] = "MAIN";
 void app_main(void)
 {
 	nvs_app_init();
-
     ESP_LOGI(main_TAG, "[APP] Startup..");
     ESP_LOGI(main_TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
     ESP_LOGI(main_TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    wifi_app_start();
 
 	recollecter_start();
 	gpios_manager_init();
     sensors_manager_init();
 	
+    mqtt_app_start();
 	status_start();
+
 	sensors_manager_sensors_startup();
 	lcd_init();
-
-	wifi_app_start();
-	mqtt_app_start();
 }
