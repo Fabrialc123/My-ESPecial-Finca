c3d98fd995baa0a74c08056cd524825f1423e644
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2023-04-10 16:04:37 +0200

    Update mq2.h

--- MESPF_Publisher/gpios/mq2.h
+++ MESPF_Publisher/gpios/mq2.h
@@ -13,17 +13,27 @@
 
 // ------------------------------------------------- Sensor information --------------------------------------------------------------
 
+// Important defines
+
 #define MQ2_N_GPIOS								1
-
 #define MQ2_N_VALUES							1
-
 #define MQ2_N_ADDITIONAL_PARAMS					0
 
-#define MQ2_LOW_V								1000
+#define MQ2_TIME_TO_UPDATE_DATA					500	// 100 = 1 second
 
+// LCD related defines (1 for each value)
+
+#define MQ2_SHOW_SMOKE_GAS_PERCENTAGE_ON_LCD	true
+
+// Additional Defines
+
+#define MQ2_LOW_V								1000
 #define MQ2_HIGH_V								4095
 
-#define MQ2_TIME_TO_UPDATE_DATA					500	// 100 = 1 second
+static const adc_bits_width_t mq2_width 		= ADC_WIDTH_BIT_12;
+static const adc_atten_t mq2_atten 				= ADC_ATTEN_DB_11;
+
+#define MQ2_N_SAMPLES   						64	//Used for minimizing noise
 
 // Structure for sensor GPIOS
 
@@ -37,47 +47,35 @@ typedef struct {
 	float smoke_gas_percentage;
 } mq2_data_t;
 
-// Value related to the LCD
+// Structure for sensor alerts
 
-#define MQ2_SHOW_SMOKE_GAS_PERCENTAGE_ON_LCD	true
-
-// Lecture properties
-
-static const adc_bits_width_t mq2_width 		= ADC_WIDTH_BIT_12;
-static const adc_atten_t mq2_atten 				= ADC_ATTEN_DB_11;
-
-#define MQ2_N_SAMPLES   						64	//Used for minimizing noise
+typedef struct {
+	bool smoke_gas_alert;
+	int smoke_gas_ticks_to_alert;
+	float smoke_gas_upper_threshold;
+	float smoke_gas_lower_threshold;
+} mq2_alerts_t;
 
 // --------------------------------------------------- Sensor management -----------------------------------------------------------
-
-/*
- * Startup MQ2 with the information previously saved (NVS)
- */
-void mq2_startup(void);
 
 /*
  * Initializes MQ2 peripheral
  */
 void mq2_init(void);
 
-/*
- * Disables MQ2 peripheral
- */
-void mq2_destroy(void);
-
 /**
  * Add a new MQ2 sensor
  *
  * NOTE: return 1 when it happens successfully, -1 in other cases
  */
-int mq2_add_sensor(int* gpios, union sensor_value_u* parameters);
+int mq2_add_sensor(int* gpios, union sensor_value_u* parameters, char* reason);
 
 /**
  * Delete a MQ2 sensor
  *
  * Note: return 1 when it happens successfully, -1 in other cases
  */
-int mq2_delete_sensor(int pos);
+int mq2_delete_sensor(int pos, char* reason);
 
 // ------------------------------------------------------- Sets --------------------------------------------------------------------
 
@@ -86,21 +84,21 @@ int mq2_delete_sensor(int pos);
  *
  * Note: return 1 when it happens successfully, -1 in other cases
  */
-int mq2_set_gpios(int pos, int* gpios);
+int mq2_set_gpios(int pos, int* gpios, char* reason);
 
 /**
  * Change parameters of a sensor
  *
  * Note: return 1 when it happens successfully, -1 in other cases
  */
-int mq2_set_parameters(int pos, union sensor_value_u* parameters);
+int mq2_set_parameters(int pos, union sensor_value_u* parameters, char* reason);
 
 /**
  * Change the alert values
  *
  * Note: return 1 when it happens successfully, -1 in other cases
  */
-int mq2_set_alert_values(int value, bool alert, int n_ticks, union sensor_value_u upper_threshold, union sensor_value_u lower_threshold);
+int mq2_set_alert_values(int value, bool alert, int n_ticks, union sensor_value_u upper_threshold, union sensor_value_u lower_threshold, char* reason);
 
 // ------------------------------------------------------- Gets --------------------------------------------------------------------
 
@@ -121,7 +119,7 @@ sensor_gpios_info_t* mq2_get_sensors_gpios(int* number_of_sensors);
 /**
  * Gets additional parameters of all sensors of this type
  *
- * Note: in case of error (or it is a sensor with no additional parameters), return NULL pointer
+ * Note: in case of error, return NULL pointer
  */
 sensor_additional_parameters_info_t* mq2_get_sensors_additional_parameters(int* number_of_sensors);
 
