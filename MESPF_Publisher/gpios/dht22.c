a4a26b5af3769a1620f8bcb7541b08311a8826ab
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2023-04-13 21:10:42 +0200

    Update dht22.c

--- MESPF_Publisher/gpios/dht22.c
+++ MESPF_Publisher/gpios/dht22.c
@@ -390,6 +390,13 @@ int dht22_add_sensor(int* gpios, union sensor_value_u* parameters, char* reason)
 		return -1;
 	}
 
+	if(!GPIO_IS_VALID_OUTPUT_GPIO(gpios[0])){
+		ESP_LOGE(TAG, "GPIO %d is input only, \"Data\" needs to be output", gpios[0]);
+		sprintf(reason, "GPIO %d is input only, \"Data\" needs to be output", gpios[0]);
+
+		return -1;
+	}
+
 	if(gpios_manager_lock_gpios(gpios, DHT22_N_GPIOS, reason) == -1)
 		return -1;
 
@@ -493,6 +500,13 @@ int dht22_set_gpios(int pos, int* gpios, char* reason){
 	if(!check_valid_pos(pos)){
 		ESP_LOGE(TAG, "Position not valid");
 		sprintf(reason, "Position not valid");
+
+		return -1;
+	}
+
+	if(!GPIO_IS_VALID_OUTPUT_GPIO(gpios[0])){
+		ESP_LOGE(TAG, "GPIO %d is input only, \"Data\" needs to be output", gpios[0]);
+		sprintf(reason, "GPIO %d is input only, \"Data\" needs to be output", gpios[0]);
 
 		return -1;
 	}
