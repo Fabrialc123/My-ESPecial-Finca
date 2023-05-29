8dae89f1f6f8eaf2f4e9549978c4157bef0a0461
rcincea <49287636+rcincea@users.noreply.github.com>
2023-05-29 18:55:57 +0200

    Delete Esp32.kt

--- MESPF_MOBILE_APP/Esp32.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tfg_boceto
-
-data class Esp32 (val nombre_esp: String, val temperatura: Int, val humedad: Int, val nivel_agua: Int,
-                  val mostrar_temperatura: Boolean, val mostrar_humedad: Boolean, val mostrar_agua: Boolean) {
-
-}
